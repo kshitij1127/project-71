e1546b50d8a8b0d8a8e3fe799b617f00daab6c3f
kshitij <gudlakshitij.1127@gmail.com>
2021-06-01 20:35:02 +0530

    second commit

--- App.js
+++ App.js
@@ -1,37 +1,51 @@
-import React from 'react'
-import { Text, View, TouchableOpacity, TextInput, Image } from 'react-native'
-import Write from './screens/WriteStoryScreen'
-import Read from './screens/ReadStoryScreen'
-import {createAppContainer} from 'react-navigation'
-import {createBottomTabNavigator} from 'react-navigation-tabs'
+import React from 'react';
+import { Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
+import Write from './screens/WriteStoryScreen';
+import Read from './screens/ReadStoryScreen';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-export default class App extends React.Component{
-
-  render(){
-    return(
-      <View>
+export default class App extends React.Component {
+  render() {
+    return ( 
         <AppContainer />
-      </View>
-    )
+    );
   }
 }
 
-const AppNavigator = createBottomTabNavigator({
-  Write: {screen: Write},
-  Read: {screen: Read},
-},{
-  defaultNavigationOptions: ({navigation}) => {
-    tabBarIcon: ({}) => {
-      const route = navigation.state.route
-      if (route === 'Write') {
-        return(
-          <Image source={require('./assets/write.png')}/>
-        )
-      }else if (route === 'Read') {
-        <Image source={require('./assets/read.png')}/>
-      }
-    }
+const AppNavigator = createBottomTabNavigator(
+  {
+    Write: { screen: Write },
+    Read: { screen: Read },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({}) => {
+        const route = navigation.state.routeName;
+        if (route === 'Write') {
+          return (
+            <Image
+              source={require('./assets/write.png')}
+              style={{
+                width: 40,
+                height: 40,
+              }}
+            />
+          );
+        } else if (route === 'Read') {
+          return (
+            <Image
+              source={require('./assets/read.png')}
+              style={{
+                width: 40,
+                height: 40,
+              }}
+            />
+          );
+        }
+      },
+    }),
   }
-})
+);
 
-const AppContainer = createAppContainer(AppNavigator)
+const AppContainer = createAppContainer(AppNavigator);
--- screens/WriteStoryScreen.js
+++ screens/WriteStoryScreen.js
@@ -6,20 +6,20 @@ export default class Write extends React.Component {
     return (
       <View style={styles.container}>
         <TextInput
-          style={(styles.displaytext, { fontSize: 24, width: 120, height: 20 })}
+          style={(styles.displaytext, { fontSize: 24, width: "100%", height: 40, marginTop: 50 })}
           placeholder="story's name"
         />
         <TextInput
           style={
             (styles.displaytext,
-            { fontSize: 24, width: 120, height: 20, marginTop: 100 })
+            { fontSize: 24, width: "100%", height: 40, marginTop: 100 })
           }
           placeholder="author"
         />
         <TextInput
           style={
             (styles.displaytext,
-            { fontSize: 18, width: 120, height: 260, marginTop: 100 })
+            { fontSize: 18, width: "100%", height: 260, marginTop: 100 })
           }
           placeholder="story"
         />
